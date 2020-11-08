60ea44fb563cca5f47b22028267794c8dffef590
khermalik <khermalik@gmail.com>
2020-11-08 11:40:18 -0800

    squares every digit of a number and concatenates them

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+// ------------------ // square every digit of a number and concatenate them
+
+// function squareDigits(num){
+//   numString = num.toString()
+//   newArray = []
+//   for (let i = 0; i < numString.length; i++) {
+//     newArray.push(numString[i] ** 2)
+//   }
+//   console.log(Number(newArray.join('')))
+// }
+// squareDigits(34567)
