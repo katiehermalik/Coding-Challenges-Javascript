8561ca1c2a1438fa93bded20179f98178cbef087
khermalik <khermalik@gmail.com>
2020-11-11 15:29:54 -0800

    returns input string, but with all five or more letter words reversed

--- scripts/app.js
+++ scripts/app.js
@@ -10,10 +10,26 @@
 // }
 // squareDigits(34567)
 
+
 // ------------------ // return the highest and lowest number
 
-function highAndLow(numbers){
-  numSet = numbers.split(' ').map(Number);
-  console.log(Math.max(...numSet) + ' ' + Math.min(...numSet));
-}
-highAndLow('3 9 45 2 5');
+// function highAndLow(numbers){
+//   numSet = numbers.split(' ').map(Number);
+//   console.log(Math.max(...numSet) + ' ' + Math.min(...numSet));
+// }
+// highAndLow('3 9 45 2 5');
+
+
+// ------------------ // Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed
+
+function spinWords(str){
+  console.log(str.split(' ').map((current) => {
+    if (current.length >= 5) {
+      return current.split('').reverse().join('')
+    } else {
+      return current
+    };
+  }).join(' '))
+};
+
+spinWords('This is another test')
