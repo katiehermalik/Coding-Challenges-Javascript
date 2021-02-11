953cd4423b43f7f1d922fb039759d2e2f553a611
khermalik <khermalik@gmail.com>
2021-02-10 17:38:06 -0800

    array methods practice

--- scripts/app.js
+++ scripts/app.js
@@ -555,38 +555,176 @@
 
 // ------------------- // Rot13 cypher
 
-function rot13(message) {
+// function rot13(message) {
 
-  const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+//   const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-  let msgArr = [];
-  let cypherIndex = [];
-  let capitalLetters = [];
-  let checkAlphabet = /[a-z]/g;
+//   let msgArr = [];
+//   let cypherIndex = [];
+//   let capitalLetters = [];
+//   let checkAlphabet = /[a-z]/g;
 
-  msgArr = message.split('').map((el) => el.toLowerCase());
-  cypherIndex = msgArr.map((el) => {
-    if (alphabet.indexOf(el) + 13 >= alphabet.length) {
-      return alphabet[13 - (alphabet.length - alphabet.indexOf(el))];
-    } else if (!el.match(checkAlphabet)) {
-      return el;
-    } else {
-      return alphabet[alphabet.indexOf(el) + 13];
-    }
-  });
+//   msgArr = message.split('').map((el) => el.toLowerCase());
+//   cypherIndex = msgArr.map((el) => {
+//     if (alphabet.indexOf(el) + 13 >= alphabet.length) {
+//       return alphabet[13 - (alphabet.length - alphabet.indexOf(el))];
+//     } else if (!el.match(checkAlphabet)) {
+//       return el;
+//     } else {
+//       return alphabet[alphabet.indexOf(el) + 13];
+//     }
+//   });
 
-  capitalLetters = message.split('').map((el, i) => {
-    if (el === el.toUpperCase()) {
-      return i;
-    }
-  });
-  capitalLetters = capitalLetters.filter((el) => el !== undefined);
+//   capitalLetters = message.split('').map((el, i) => {
+//     if (el === el.toUpperCase()) {
+//       return i;
+//     }
+//   });
+//   capitalLetters = capitalLetters.filter((el) => el !== undefined);
 
-  for (let i = 0; i < capitalLetters.length; i++) {
-    cypherIndex[capitalLetters[i]] = cypherIndex[
-      capitalLetters[i]
-    ].toUpperCase();
-  }
+//   for (let i = 0; i < capitalLetters.length; i++) {
+//     cypherIndex[capitalLetters[i]] = cypherIndex[
+//       capitalLetters[i]
+//     ].toUpperCase();
+//   }
 
-  return cypherIndex.join('');
-}
+//   return cypherIndex.join('');
+// }
+
+// -------------------------------- // Array Methods Practice
+
+const texasss = [
+  {
+    name: 'Mike',
+    age: 23,
+    gender: 'm',
+    us: false,
+  },
+  {
+    name: 'Liz',
+    age: 20,
+    gender: 'f',
+    us: true,
+  },
+  {
+    name: 'Chris',
+    age: 102,
+    gender: 'm',
+    us: true,
+  },
+  {
+    name: 'Chuloo',
+    age: 27,
+    gender: 'm',
+    us: false,
+  },
+  {
+    name: 'Annie',
+    age: 30,
+    gender: 'f',
+    us: true,
+  },
+]
+
+// Part 1 - Find all users older than 24
+console.log(texasss.filter((val) => val.age > 24))
+
+// Part 2 - Find the total age of all users
+console.log(texasss.reduce((acc, curr) => acc + curr.age, 0))
+
+// Part 3 - List all female coders
+console.log(texasss.filter((val) => val.gender === 'f'))
+
+
+// ARRAY 2
+const newieyork = [
+  {
+    name: 'Michelle',
+    age: 19,
+    coder:true,
+    gender: 'f',
+    us: true,
+  },
+  {
+    name: 'Sam',
+    age: 25,
+    coder:false,
+    gender: 'm',
+    us: false,
+  },
+  {
+    name: 'Ivy',
+    age: 26,
+    coder:true,
+    gender: 'f',
+    us: false,
+  },
+  {
+    name: 'Nick',
+    age: 32,
+    coder:true,
+    gender: 'm',
+    us: true,
+  },
+  {
+    name: 'Jim Beglin',
+    age: 65,
+    coder:false,
+    gender: 'm',
+    us: true,
+  },
+]
+
+// Part 1 - List all users in US in ascending order
+console.log((newieyork.filter((val) => val.us === true))
+  .sort((a, b) => a.name.localeCompare(b.name)))
+
+// Part 2 - Sort all users by age
+console.log(newieyork.sort((a, b) => a.age - b.age));
+
+// Part 3 -  List all female coders
+console.log(newieyork.filter((val) => val.gender === 'f'))
+
+// ARRAY 3
+const vegzas = [
+  {
+    name: 'Charly',
+    age: 32,
+    coder:true,
+    gender: 'm',
+  },
+  {
+    name: 'Law',
+    age: 21,
+    coder:true,
+    gender: 'm',
+  },
+  {
+    name: 'Rosey',
+    age: 42,
+    coder:false,
+    gender: 'f',
+  },
+  {
+    name: 'Steph',
+    age: 18,
+    coder:true,
+    gender:'f'
+  },
+  {
+    name: 'Jon',
+    age: 47,
+    coder:false,
+    gender: 'm',
+  },
+]
+
+// Part 1 - Find the total age of male coders under 25
+console.log(vegzas.filter((val) => val.gender === 'm' && val.age < 25)
+  .reduce((acc, curr) => acc + curr.age, 0))
+
+// Part 2 - List all male coders over 30
+console.log(vegzas.filter((val) => val.gender === 'm' && val.age > 30))
+
+// Part 3 - Find the total age of everyone in texasss, newieyork and vegzas combined.
+console.log([...texasss, ...newieyork, ...vegzas].reduce((acc, curr) => acc + curr.age, 0))
